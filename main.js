41ac2446ac1467a7110553c2633257b4167f6ea9
Таня Дашипилова <tanya.volk01@mail.ru>
2024-04-24 20:38:35 +0800

    +

---
+++ main.js
@@ -0,0 +1,17 @@
+function get(array, index, defaultValue = null) {
+    if(array.at(index) !== undefined ){
+       console.log(  array.at(index) );
+    }
+    else {
+        console.log(  defaultValue);
+    }
+  }
+  const cities = ['moscow', 'london', 'berlin', 'porto', '', null, undefined]; 
+
+console.log(get(cities, 1)); // 'london'
+console.log(get(cities, 4)); // ''
+console.log(get(cities, 10, 'paris')); // 'paris'
+console.log(get(cities, -1, 'oops')); // 'oops'
+console.log(get(cities, 5, 'oops')); // null
+console.log(get(cities, 6, 'oops')); // undefined
+console.log(get(cities, 7)); // null
