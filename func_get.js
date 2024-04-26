fe0f1b327ab5f5507ee09844080d8b3d31ff6ee8
Таня Дашипилова <tanya.volk01@mail.ru>
2024-04-26 22:29:21 +0800

    +попытка экспорта

---
+++ func_get.js
@@ -0,0 +1,9 @@
+export default function get(array, index, defaultValue = null) {
+    if(array.at(index) !== undefined ){
+       return   array.at(index) 
+    }
+    else {
+        return   defaultValue
+    }
+  }
+//  const cities = ['moscow', 'london', 'berlin', 'porto', '', null, undefined]; 
--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-function get(array, index, defaultValue = null) {
-    if(array.at(index) !== undefined ){
-       return   array.at(index) 
-    }
-    else {
-        return   defaultValue
-    }
-  }
-  const cities = ['moscow', 'london', 'berlin', 'porto', '', null, undefined]; 
+import get from './func_get.js';
+
+/* const cities = ['moscow', 'london', 'berlin', 'porto', '', null, undefined]; 
 
 console.log(get(cities, 1)); // 'london'
 console.log(get(cities, 4)); // ''
@@ -14,4 +8,5 @@ console.log(get(cities, 10, 'paris')); // 'paris'
 console.log(get(cities, -1, 'oops')); // 'oops'
 console.log(get(cities, 5, 'oops')); // null
 console.log(get(cities, 6, 'oops')); // undefined
-console.log(get(cities, 7)); // null
+console.log(get(cities, 7)); // null  
+*/
