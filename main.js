a0f35b836bc1a1f8e166c95fd8a9f0c0424f27b8
Таня Дашипилова <tanya.volk01@mail.ru>
2024-05-23 08:55:46 +0800

    тсправила

--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-//import get from './func_get.js';
-/*
-const cities = require('./func_get');
-console.log(cities.get(10, 'paris')); // 'paris'
-*/
-
-/* const cities = ['moscow', 'london', 'berlin', 'porto', '', null, undefined]; 
-
-console.log(get(cities, 1)); // 'london'
-console.log(get(cities, 4)); // ''
-console.log(get(cities, 10, 'paris')); // 'paris'
-console.log(get(cities, -1, 'oops')); // 'oops'
-console.log(get(cities, 5, 'oops')); // null
-console.log(get(cities, 6, 'oops')); // undefined
-console.log(get(cities, 7)); // null  
-*/
-
 const func = require('./func_get');
 const cities = ['moscow', 'london', 'berlin', 'porto', '', null, undefined]; 
 
